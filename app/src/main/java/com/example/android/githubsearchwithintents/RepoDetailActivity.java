366e781a81ebb682edc0226c10535cba8913c203
Rob Hess <robwhess@gmail.com>
2020-02-06 15:48:17 -0800

    Add code from 2/6 lecture.

--- app/src/main/java/com/example/android/githubsearchwithintents/RepoDetailActivity.java
+++ app/src/main/java/com/example/android/githubsearchwithintents/RepoDetailActivity.java
@@ -1,15 +1,25 @@
 package com.example.android.githubsearchwithintents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.android.githubsearchwithintents.data.GitHubRepo;
 
+import java.util.List;
+
 public class RepoDetailActivity extends AppCompatActivity {
     public static final String EXTRA_GITHUB_REPO = "GitHubRepo";
+
+    private GitHubRepo mRepo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +28,61 @@ public class RepoDetailActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra(EXTRA_GITHUB_REPO)) {
-            GitHubRepo repo = (GitHubRepo)intent.getSerializableExtra(EXTRA_GITHUB_REPO);
+            mRepo = (GitHubRepo)intent.getSerializableExtra(EXTRA_GITHUB_REPO);
 
             TextView repoNameTV = findViewById(R.id.tv_repo_name);
-            repoNameTV.setText(repo.full_name);
+            repoNameTV.setText(mRepo.full_name);
 
             TextView repoDescriptionTV = findViewById(R.id.tv_repo_description);
-            repoDescriptionTV.setText(repo.description);
+            repoDescriptionTV.setText(mRepo.description);
 
             TextView repoStarsTV = findViewById(R.id.tv_repo_stars);
-            repoStarsTV.setText(Integer.toString(repo.stargazers_count));
+            repoStarsTV.setText(Integer.toString(mRepo.stargazers_count));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.repo_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_view_repo:
+                viewRepoOnWeb();
+                return true;
+            case R.id.action_share:
+                shareRepo();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void viewRepoOnWeb() {
+        if (mRepo != null) {
+            Uri repoUri = Uri.parse(mRepo.html_url);
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, repoUri);
+
+            PackageManager pm = getPackageManager();
+            List<ResolveInfo> activities = pm.queryIntentActivities(webIntent, PackageManager.MATCH_DEFAULT_ONLY);
+            if (activities.size() > 0) {
+                startActivity(webIntent);
+            }
+        }
+    }
+
+    private void shareRepo() {
+        if (mRepo != null) {
+            String shareText = mRepo.full_name + ": " + mRepo.html_url;
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareText);
+            shareIntent.setType("text/plain");
+
+            Intent chooserIntent = Intent.createChooser(shareIntent, null);
+            startActivity(chooserIntent);
         }
     }
 }
